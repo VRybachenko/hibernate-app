81a1ce51e22f430cde7d058b59150401deda60ae
vrybachenko <vrybache@softserveinc.com>
2023-05-18 17:29:59 +0300

    add entities and get ID in the database via Hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,10 +18,13 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 2);
-            session.delete(person);
+            Person person = new Person("Some name", 24);
+            session.save(person);
 
             session.getTransaction().commit();
+
+            System.out.println(person.getId());
+
         } finally {
             sessionFactory.close();
         }
